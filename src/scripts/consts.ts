9cfe037089111dee3642fa099873d2c283c69e32
bogolepov <bogolepov@gmail.com>
2024-08-29 00:48:23 +0200

    1. Website-structure is developed. 2. Header is made

---
+++ src/scripts/consts.ts
@@ -0,0 +1,3 @@
+export const HAUSVERWALTUNG: string = 'Hausverwaltung';
+export const DAHM_HAUSVERWALTUNG: string = 'Hausverwaltung Dahm';
+export const DAHM_HAUSVERWALTUNG_AUS_HAGEN: string = 'Hausverwaltung Dahm aus Hagen';

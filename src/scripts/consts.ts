1c48f5dc029b43239061777f5b5ca5af776f90c8
bogolepov <bogolepov@gmail.com>
2024-08-31 03:04:00 +0200

    Many changes 1. Pages content 2. Astro Icon (!!!) added 3.Head

--- src/scripts/consts.ts
+++ src/scripts/consts.ts
@@ -1,3 +1,1 @@
-export const HAUSVERWALTUNG: string = 'Hausverwaltung';
-export const DAHM_HAUSVERWALTUNG: string = 'Hausverwaltung Dahm';
-export const DAHM_HAUSVERWALTUNG_AUS_HAGEN: string = 'Hausverwaltung Dahm aus Hagen';
+export const HAUSVERWALTUNG_DAHM: string = 'Hausverwaltung Dahm';
